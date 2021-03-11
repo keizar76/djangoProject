36eb573ffbcca763ae4178a356abf8c707dd943c
keizar76 <31140659+keizar76@users.noreply.github.com>
2021-03-10 22:23:54 -0500

    Modificaciones

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -17,8 +17,8 @@ Including another URLconf
 
 from django.urls import path,include
 from django.contrib import admin
-from django.config import settings
-from django.config.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
